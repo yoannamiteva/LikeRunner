226796a00af279c4226d4b4a0e4620a6266c88b9
Yoanna Miteva <yoanna1394@abv.bg>
2016-04-10 19:26:52 +0300

    Added files via upload

--- resources/views/top100.blade.php
+++ resources/views/top100.blade.php
@@ -5,10 +5,10 @@
 	<div class="row">
 		<div class="col-sm-2">
 			<div>
-				<a href="shop.html"><img src="files/bag.png"></a>
+				<a href="shop"><img src="files/bag.png"></a>
 			</div>
 			<div>
-				<a href="top100.html"><img src="files/top.png"></a>
+				<a href="top"><img src="files/top.png"></a>
 			</div>
 		</div>
 		<div class="col-sm-8">
@@ -22,19 +22,27 @@
 				    </tr>
 				  </thead>
 				  <tbody>
+				   @foreach($users as $user)
 				    <tr>
-				      <td>1</td>
-				      <td>Ivan</td>
-				      <td>1210</td>
+				      <td>{{$counter++}}</td>
+				      <td>{{$user->name}}</td>
+				      <td>{{$user->likes}}</td>
 				    </tr>
+				    @endforeach
 				  </tbody>
 				</table> 
 			</div>
 		</div>
 		
 		<div class="col-sm-2 ">
-			<h2>Money <span class="badge">100</span></h2>
-			<h2>Likes <span class="badge">0</span></h2>
+			<div>
+				<img class="money" src="files/coins.png">
+				<h2>Money <span class="badge">100</span></h2>
+			</div>
+			<div>
+				<img class="likes" src="files/like.png">
+				<h2>Likes <span class="badge">0</span></h2>
+			</div>
 		</div>
 	</div>
 </div>
