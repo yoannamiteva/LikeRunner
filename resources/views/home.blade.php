f7addb668aeb7c3d1ab0403f3b81118ff4cde232
Yoanna Miteva <yoanna1394@abv.bg>
2016-04-10 19:26:12 +0300

    Added files via upload

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,21 @@
 	<div class="row">
 		<div class="col-sm-2">
 			<div>
-				<a href="shop.html"><img src="files/bag.png"></a>
+				<a href="shop"><img src="files/bag.png"></a>
 			</div>
 			<div>
-				<a href="top100.html"><img src="files/top.png"></a>
+				<a href="top"><img src="files/top.png"></a>
 			</div>
 		</div>
 		<div class="col-sm-2 col-sm-offset-8">
-			<h2>Money <span class="badge">100</span></h2>
-			<h2>Likes <span class="badge">0</span></h2>
+			<div>
+				<img class="money" src="files/coins.png">
+				<h2>Money <span class="badge">100</span></h2>
+			</div>
+			<div>
+				<img class="likes" src="files/like.png">
+				<h2>Likes <span class="badge">0</span></h2>
+			</div>
 		</div>
 	</div>
 	<div class="row">
@@ -23,7 +29,7 @@
 					<h3>Run for more Likes! You must be the first in top100</h3>
 					
 				</div>
-				<a href="#" class="btn btn-danger btn-lg">Start Game</a>
+				<a href="game" class="btn btn-danger btn-lg">Start Game</a>
 		
 			</div>
 	
