5b80525d57406f193cb82a9105fa6f6cd31e326b
Yoanna Miteva <yoanna1394@abv.bg>
2016-04-10 17:34:13 +0300

    Added files via upload

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,62 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+use App\Item;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
 class ShopController extends Controller
 {
-	public function __construct()
+public function __construct()
 	{
 		$this->middleware('auth');
 	}
 	
-	public function addItem ($itemId){
-	
-		$shop = Shop::where('user_id',Auth::user()->id)->first();
-	
+	public function addItem ($item)
+	{
 		$shopItem  = new Item();
-		$shopItem->id=$itemId;
+		$shopItem->id = $item->id;
 		$shopItem->save();
 	
 		return redirect('/shop');
-	
 	}
 	
-	public function showShop(){
-		$shop = Shop::where('user_id',Auth::user()->id)->first();
-	
-		$items = $shop->shopItems;
-		$total=0;
-		foreach($items as $item){
-			$total+=$item->item->price;
-		}
-	
-		return view('shop',['items'=>$items,'total'=>$total]);
+	public function showShop()
+	{
+		//$user = Auth::user;
+		$items = Item::all();
+		return view('shop' , compact('items'));
 	}
 	
-	public function removeItem($id){
-	
+	public function removeItem($id)
+	{
 		Item::destroy($id);
 		return redirect('/shop');
 	}
 	
-	public function update(Request $request, $id)
-	{
-		if (!$request->name or !$request->price or !$request->description or !$request->img_address) {
-			return Response::json([
-					'error' => [
-							'message' => 'Please Provide name, price, description and image!'
-					]
-			], 422);
-		}
-		$item = Item::find($id);
-		$item->name = $request->name;
-		$item->price = $request->price;
-		$item->description = $request->description;
-		$item->img_address = $request->img_address;
-		$item->save();
-		return $item;
-	}
 }
