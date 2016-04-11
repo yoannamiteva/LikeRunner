e6906976b556ffcdc091d3a164e44f6919a6072e
Yoanna Miteva <yoanna1394@abv.bg>
2016-04-11 10:53:01 +0300

    Added files via upload

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -2,55 +2,56 @@
 
 @section('content')
  
-    <div class="row">
-        <div class="col-sm-8">
-						<div>
-					<form class="form-horizontal" role="form" method="POST" action="">    
-	                	{!! csrf_field() !!}   
-	                	{{ method_field('PATCH')}}       
-							<table class="table table-striped table-hover ">
-							  <thead>
-							    <tr>
-							      <th>#</th>
-							      <th>Item</th>
-							      <th>Price</th>
-							      <th>Description</th>
-							    </tr>
-							  </thead>
-							  <tbody>
-                @foreach($items as $item)
+ <div class="container col-sm-12 ">
+	<div class="row">
+		<div class="col-sm-2">
+			<div>
+				<a href="shop"><img src="files/bag.png"></a>
+			</div>
+			<div>
+				<a href="top"><img src="files/top.png"></a>
+			</div>
+		</div>
+		<div class="col-sm-8">      
+			<table class="table table-bordered panel panel-default">
+				  <thead class="panel-heading">  
+				    <tr>
+				      <th>Item</th>
+				      <th>Price</th>
+				      <th>Description</th>
+				      <th> </th>
+				    </tr>
+				  </thead>
+							  
+				 <tbody class="panel-body">
+                  @foreach($items as $item)
                     <tr>
+                        <td>
+                            
+                                <img class="media-object" src="{{$item->img_address}}" style="width: 100px; height: 72px;">
+                                <p class="navbar-brand">{{$item->name}}</p>
+                               
+                         </td>
+                        <td><strong>{{$item->price}} $</strong></td>
+                        <td><strong>{{$item->description}} </strong></td>
                         <td >
-                            <div >
-                                <a href="#"> <img class="media-object" src="{{$item->img_address}}" style="width: 100px; height: 72px;"> </a>
-                                <div>
-                                    <h4 ><a href="#">{{$item->name}}</a></h4>
-                                </div>
-                            </div></td>
-                        <td  style="text-align: center">
-                        </td>
-                        <td ></td>
-                        <td ><strong>{{$item->price}} $</strong></td>
-                        <td ><strong>{{$item->description}} </strong></td>
-                        <td >
-                            <a href="/removeItem/{{$item->id}}"> <button type="button" class="btn btn-danger">
-                                    <span class="fa fa-remove"></span> Remove
-                                </button>
-                            </a>
-                            <a href="/buyItem/{{$item->id}}"> <button type="button" class="btn btn-info">
-                                    <span class="fa fa-shopping-cart"></span> Buy
-                                </button>
-                            </a>
+                           
+                           <a class="btn icon-btn btn-success" href="/buyItem/{{$item->id}}">
+							<span class="glyphicon btn-glyphicon glyphicon-plus img-circle text-success"></span>
+								Add
+						   </a>
+                            
+                          <a class="btn icon-btn btn-warning" href="/removeItem/{{$item->id}}">
+							<span class="glyphicon btn-glyphicon glyphicon-minus img-circle text-warning"></span>
+								Remove
+						 </a>
+
+                          
                         </td>
                     </tr>
                 @endforeach
  
-                <tr>
-                    <td>   </td>
-                    <td>   </td>
-                    <td>   </td>
-             
-                </tr>
+              
                 <tr>
                     <td>   </td>
                     <td>   </td>
@@ -64,7 +65,18 @@
                 </tr>
                 </tbody>
             </table>
-            </form>
-        </div>
-    </div>
+		</div>
+		
+		<div class="col-sm-2 ">
+			<div>
+				<img class="money" src="files/coins.png">
+				<h2>Money <span class="badge">{{$auth->money}}</span></h2>
+			</div>
+			<div>
+				<img class="likes" src="files/like.png">
+				<h2>Likes <span class="badge">{{$auth->likes}}</span></h2>
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
