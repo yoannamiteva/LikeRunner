bc114b9ea365e916884f16db9ac7fedc23513c17
Yoanna Miteva <yoanna1394@abv.bg>
2016-04-11 19:07:29 +0300

    Added files via upload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,15 +46,16 @@
                 </button>
 
                 <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <p class="navbar-brand" >
                     Like Runner
-                </a>
+                </p>
             </div>
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
                     <li><a href="{{ url('/home') }}">Home</a></li>
+                    <li><a href="{{ url('/game') }}">Star Game</a></li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
